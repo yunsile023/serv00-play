4d9acd70f35b62c41aa61ba05ddc0e4647e8b576
yunsile023 <53665983+yunsile023@users.noreply.github.com>
2025-01-27 06:01:52 +0800

    Update tu2.sh

--- ssl/tu2.sh
+++ ssl/tu2.sh
@@ -1,131 +1,54 @@
 #!/bin/bash
 export LC_ALL=C
-export UUID=${UUID:-'760ce91d-8147-412c-9b21-92e172d0e46e'}         
+export UUID=${UUID:-'39e8b439-06be-4783-ad52-6357fc5e8743'}         
 export NEZHA_SERVER=${NEZHA_SERVER:-''}             
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}            
 export NEZHA_KEY=${NEZHA_KEY:-''}
-export PASSWORD=${PASSWORD:-'admin'} 
-export PORT=${PORT:-''}  
+export PASSWORD=${PASSWORD:-'admin'}
+export PORT1=${PORT1:-''}  
+export PORT2=${PORT2:-''}  
+export PORT3=${PORT3:-''}  
 export CHAT_ID=${CHAT_ID:-''} 
 export BOT_TOKEN=${BOT_TOKEN:-''} 
 export SUB_TOKEN=${SUB_TOKEN:-'sub'}
+
 HOSTNAME=$(hostname)
 USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
+
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="$HOME/domains/${USERNAME}.ct8.pl/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.ct8.pl/public_html" || WORKDIR="$HOME/domains/${USERNAME}.serv00.net/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.serv00.net/public_html"
+
 rm -rf "$WORKDIR" && mkdir -p "$WORKDIR" "$FILE_PATH" && chmod 777 "$WORKDIR" "$FILE_PATH" >/dev/null 2>&1
+
 bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
 
-# Function to generate a random port number
-generate_random_port() {
-    shuf -i 10000-65535 -n 1
+generate_ports() {
+  while true; do
+    PORT1=$(shuf -i 10000-65535 -n 1)
+    PORT2=$(shuf -i 10000-65535 -n 1)
+    PORT3=$(shuf -i 10000-65535 -n 1)
+    if [[ "$PORT1" -ne "$PORT2" && "$PORT1" -ne "$PORT3" && "$PORT2" -ne "$PORT3" ]]; then
+      break
+    fi
+  done
 }
 
-check_binexec_and_port () {
-  port_list=$(devil port list)
-  tcp_ports=$(echo "$port_list" | grep -c "tcp")
-  udp_ports=$(echo "$port_list" | grep -c "udp")
-
-  if [[ $udp_ports -lt 1 ]]; then
-      echo -e "\e[1;91m没有可用的UDP端口,正在调整...\e[0m"
-
-      if [[ $tcp_ports -ge 3 ]]; then
-          tcp_port_to_delete=$(echo "$port_list" | awk '/tcp/ {print $1}' | head -n 1)
-          devil port del tcp $tcp_port_to_delete
-          echo -e "\e[1;32m已删除TCP端口: $tcp_port_to_delete\e[0m"
-      fi
-
-      while true; do
-          udp_port=$(generate_random_port)
-          result=$(devil port add udp $udp_port 2>&1)
-          if [[ $result == *"succesfully"* ]]; then
-              echo -e "\e[1;32m已添加UDP端口: $udp_port"
-              udp_port1=$udp_port
-              break
-          else
-              echo -e "\e[1;33m端口 $udp_port 不可用，尝试其他端口...\e[0m"
-          fi
-      done
-
-      echo -e "\e[1;32m端口已调整完成, 将断开SSH连接, 请重新连接SSH并重新执行脚本\e[0m"
-      devil binexec on >/dev/null 2>&1
-      kill -9 $(ps -o ppid= -p $$) >/dev/null 2>&1
-  else
-      udp_ports=$(echo "$port_list" | awk '/udp/ {print $1}')
-      udp_port1=$(echo "$udp_ports" | sed -n '1p')
-
-      echo -e "\e[1;35m当前UDP端口: $udp_port1\e[0m"
-  fi
-
-  export PORT=$udp_port1
-}
-
-check_binexec_and_port
+generate_ports
+export PORT1
+export PORT2
+export PORT3
 
 clear
 echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
-ARCH=$(uname -m) && DOWNLOAD_DIR="." && mkdir -p "$DOWNLOAD_DIR" && FILE_INFO=()
-if [ "$ARCH" == "arm" ] || [ "$ARCH" == "arm64" ] || [ "$ARCH" == "aarch64" ]; then
-    FILE_INFO=("https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-x86_64-unknown-freebsd.sha256sum web" "https://github.com/eooce/test/releases/download/ARM/swith npm")
-elif [ "$ARCH" == "amd64" ] || [ "$ARCH" == "x86_64" ] || [ "$ARCH" == "x86" ]; then
-    FILE_INFO=("https://github.com/etjec4/tuic/releases/download/tuic-server-1.0.0/tuic-server-1.0.0-x86_64-unknown-freebsd web" "https://github.com/eooce/test/releases/download/freebsd/npm npm")
-else
-    echo "Unsupported architecture: $ARCH"
-    exit 1
-fi
-declare -A FILE_MAP
-generate_random_name() {
-    local chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890
-    local name=""
-    for i in {1..6}; do
-        name="$name${chars:RANDOM%${#chars}:1}"
-    done
-    echo "$name"
-}
 
-download_with_fallback() {
-    local URL=$1
-    local NEW_FILENAME=$2
-
-    curl -L -sS --max-time 2 -o "$NEW_FILENAME" "$URL" & 
-    CURL_PID=$!
-    CURL_START_SIZE=$(stat -c%s "$NEW_FILENAME" 2>/dev/null || echo 0)
-    
-    sleep 1
-
-    CURL_CURRENT_SIZE=$(stat -c%s "$NEW_FILENAME" 2>/dev/null || echo 0)
-    
-    if [ "$CURL_CURRENT_SIZE" -le "$CURL_START_SIZE" ]; then
-        kill $CURL_PID 2>/dev/null
-        wait $CURL_PID 2>/dev/null
-        wget -q -O "$NEW_FILENAME" "$URL" 2>/dev/null
-        echo -e "\e[1;32mDownloading $NEW_FILENAME by wget\e[0m"
-    else
-        wait $CURL_PID 2>/dev/null
-        echo -e "\e[1;32mDownloading $NEW_FILENAME by curl\e[0m"
-    fi
-}
-
-for entry in "${FILE_INFO[@]}"; do
-    URL=$(echo "$entry" | cut -d ' ' -f 1)
-    RANDOM_NAME=$(generate_random_name)
-    NEW_FILENAME="$DOWNLOAD_DIR/$RANDOM_NAME"
-    
-    download_with_fallback "$URL" "$NEW_FILENAME"
-    
-    chmod +x "$NEW_FILENAME"
-    FILE_MAP[$(echo "$entry" | cut -d ' ' -f 2)]="$NEW_FILENAME"
-done
-wait
-
-# Generate cert
+# Generate certificate
 openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout $WORKDIR/server.key -out $WORKDIR/server.crt -subj "/CN=bing.com" -days 36500
 
-# Create configuration file and generate 3 different TUIC configurations
-for i in {1..3}; do
-    RANDOM_PORT=$(generate_random_port)
-    cat > config_$i.json <<EOL
+# Generate configuration files
+for PORT in $PORT1 $PORT2 $PORT3; do
+  CONFIG_FILE="config_${PORT}.json"
+  cat > $WORKDIR/$CONFIG_FILE <<EOL
 {
-  "server": "[::]:$RANDOM_PORT",
+  "server": "[::]:$PORT",
   "users": {
     "$UUID": "$PASSWORD"
   },
@@ -145,17 +68,70 @@ for i in {1..3}; do
   "log_level": "warn"
 }
 EOL
-    cat > ${FILE_PATH}/${SUB_TOKEN}_tuic_$i.log <<EOF
-tuic://$UUID:$PASSWORD@$HOST_IP:$RANDOM_PORT?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$NAME-$i
+done
+
+# Function to install keepalive service
+install_keepalive() {
+    echo -e "\n\e[1;35m正在安装保活服务中,请稍等......\e[0m"
+    keep_path="$HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs"
+    [ -d "$keep_path" ] || mkdir -p "$keep_path"
+    app_file_url="https://tuic.2go.us.kg/app.js"
+
+    if command -v curl &> /dev/null; then
+        curl -s -o "${keep_path}/app.js" "$app_file_url"
+    elif command -v wget &> /dev/null; then
+        wget -q -O "${keep_path}/app.js" "$app_file_url"
+    else
+        echo -e "\n\e[1;33m警告: 文件下载失败,请手动从https://tuic.2go.us.kg/app.js下载文件,并将文件上传到${keep_path}目录下\e[0m"
+        return
+    fi
+
+    cat > ${keep_path}/.env <<EOF
+UUID=${UUID}
+SUB_TOKEN=${SUB_TOKEN}
+TELEGRAM_CHAT_ID=${CHAT_ID}
+TELEGRAM_BOT_TOKEN=${BOT_TOKEN}
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
 EOF
-    echo -e "\e[1;32m生成了TUIC配置文件: ${FILE_PATH}/${SUB_TOKEN}_tuic_$i.log\e[0m"
+
+    devil www add keep.${USERNAME}.serv00.net nodejs /usr/local/bin/node18 > /dev/null 2>&1
+    devil ssl www add $HOST_IP le le keep.${USERNAME}.serv00.net > /dev/null 2>&1
+    cd ${keep_path} && npm install dotenv axios --silent > /dev/null 2>&1
+
+    devil www restart keep.${USERNAME}.serv00.net 2>&1 && echo -e "\e[1;32m\n全自动保活服务安装成功\n\e[0m" || echo -e "\e[1;91m安装失败，请重试\e[0m"
+}
+
+# Output the configuration links
+HOST_IP="127.0.0.1"
+echo -e "\e[1;32m本机IP: $HOST_IP\033[0m"
+
+for PORT in $PORT1 $PORT2 $PORT3; do
+  CONFIG_FILE="${FILE_PATH}/${SUB_TOKEN}_tuic_${PORT}.log"
+  NAME="$HOST_IP-TUIC-$PORT"
+
+  cat > $CONFIG_FILE <<EOF
+tuic://$UUID:$PASSWORD@$HOST_IP:$PORT?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$NAME
+EOF
+
+  echo -e "\n\e[1;33mTUIC 配置: \033[0m\n"
+  cat << EOF
+- name: $NAME
+  type: tuic
+  server: $HOST_IP
+  port: $PORT
+  uuid: $UUID
+  password: $PASSWORD
+  alpn: [h3]
+  disable-sni: true
+  udp-relay-mode: native
+  congestion-controller: bbr
+  sni: www.bing.com
+  skip-cert-verify: true
+EOF
+
 done
 
-# Output the subscription links
-echo -e "\e[1;35m三个不同端口的TUIC配置订阅链接: \033[0m"
-for i in {1..3}; do
-    echo -e "\e[1;35mhttps://${USERNAME}.serv00.net/${SUB_TOKEN}_tuic_$i.log  适用于V2RayN、Nekobox等\e[0m"
-done
-
-echo -e "\n\e[1;35m节点订阅链接已生成，分别对应三个不同的端口，直接导入V2rayN等工具即可使用。\033[0m"
-rm -rf config_*.json fake_useragent_0.2.0-linux-x64.tgz
+install_keepalive
+echo -e "\e[1;32m脚本执行完成\033[0m"
