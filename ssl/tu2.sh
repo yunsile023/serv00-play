f64652b6cd01d7b6669e7f6b639b6cdd10f648e3
yunsile023 <53665983+yunsile023@users.noreply.github.com>
2025-01-27 05:43:12 +0800

    Update tu2.sh

--- ssl/tu2.sh
+++ ssl/tu2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export LC_ALL=C
-export UUID=${UUID:-'39e8b439-06be-4783-ad52-6357fc5e8743'}         
+export UUID=${UUID:-'760ce91d-8147-412c-9b21-92e172d0e46e'}         
 export NEZHA_SERVER=${NEZHA_SERVER:-''}             
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}            
 export NEZHA_KEY=${NEZHA_KEY:-''}
