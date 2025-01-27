b4b75c098c48c23ce16d5141f779e3884e338d72
yunsile023 <53665983+yunsile023@users.noreply.github.com>
2025-01-27 08:24:27 +0800

    Update tu2.sh

--- ssl/tu2.sh
+++ ssl/tu2.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 export LC_ALL=C
-export UUID=${UUID:-'87bf5ca0-6d29-434e-bf3c-4ef66e68d1c8'}
-export NEZHA_SERVER=${NEZHA_SERVER:-''}
-export NEZHA_PORT=${NEZHA_PORT:-'5555'}
+export UUID=${UUID:-'87bf5ca0-6d29-434e-bf3c-4ef66e68d1c8'}         
+export NEZHA_SERVER=${NEZHA_SERVER:-''}             
+export NEZHA_PORT=${NEZHA_PORT:-'5555'}            
 export NEZHA_KEY=${NEZHA_KEY:-''}
-export PASSWORD=${PASSWORD:-'admin'}
+export PASSWORD=${PASSWORD:-'admin'} 
+export CHAT_ID=${CHAT_ID:-'7816805338'} 
+export BOT_TOKEN=${BOT_TOKEN:-'7904127530:AAEvop-tjPB9C_yNTsdjuNDwIN5oiuqNtxk'} 
+export SUB_TOKEN=${SUB_TOKEN:-'sub'}
 PORTS=(49477 51996 61757)  # 定义三个端口
-USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-
-[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
-[ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 777 "$WORKDIR" && cd "$WORKDIR")
+USERNAME=$(whoami | tr '[:upper:]' '[:lower:]')
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="$HOME/domains/${USERNAME}.ct8.pl/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.ct8.pl/public_html" || WORKDIR="$HOME/domains/${USERNAME}.serv00.net/logs" && FILE_PATH="${HOME}/domains/${USERNAME}.serv00.net/public_html"
+rm -rf "$WORKDIR" && mkdir -p "$WORKDIR" "$FILE_PATH" && chmod 777 "$WORKDIR" "$FILE_PATH" >/dev/null 2>&1
 bash -c 'ps aux | grep $(whoami) | grep -v "sshd\|bash\|grep" | awk "{print \$2}" | xargs -r kill -9 >/dev/null 2>&1' >/dev/null 2>&1
+
 
 clear
 echo -e "\e[1;35m正在安装中,请稍等...\e[0m"
@@ -99,13 +102,6 @@ EOL
 
 echo -e "\e[1;32mGenerated config_$PORT.json\e[0m"
 done
-
-# Run multiple instances
-for PORT in "${PORTS[@]}"; do
-    nohup ./${FILE_MAP[web]} -c "config_$PORT.json" >$WORKDIR/log_tuic_$PORT.log 2>&1 &
-    echo -e "\e[1;32mInstance running on port $PORT\e[0m"
-done
-
 # Output links for V2RayN
 for PORT in "${PORTS[@]}"; do
     V2RAY_LINK="tuic://$UUID%3A$PASSWORD@$HOSTNAME:$PORT?sni=www.bing.com&alpn=h3&congestion_control=bbr#PL-${HOSTNAME}-tuic"
@@ -124,3 +120,148 @@ for PORT in "${PORTS[@]}"; do
 done
 
 echo -e "\e[1;32mAll instances are running successfully!\e[0m"
+
+# Run multiple instances
+for PORT in "${PORTS[@]}"; do
+    nohup ./${FILE_MAP[web]} -c "config_$PORT.json" >$WORKDIR/log_tuic_$PORT.log 2>&1 &
+    echo -e "\e[1;32mInstance running on port $PORT\e[0m"
+done
+
+install_keepalive () {
+    echo -e "\n\e[1;35m正在安装保活服务中,请稍等......\e[0m"
+    keep_path="$HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs"
+    [ -d "$keep_path" ] || mkdir -p "$keep_path"
+    app_file_url="https://tuic.2go.us.kg/app.js"
+
+    if command -v curl &> /dev/null; then
+        curl -s -o "${keep_path}/app.js" "$app_file_url"
+    elif command -v wget &> /dev/null; then
+        wget -q -O "${keep_path}/app.js" "$app_file_url"
+    else
+        echo -e "\n\e[1;33m警告: 文件下载失败,请手动从https://tuic.2go.us.kg/app.js下载文件,并将文件上传到${keep_path}目录下\e[0m"
+        return
+    fi
+
+    cat > ${keep_path}/.env <<EOF
+UUID=${UUID}
+SUB_TOKEN=${SUB_TOKEN}
+TELEGRAM_CHAT_ID=${CHAT_ID}
+TELEGRAM_BOT_TOKEN=${BOT_TOKEN}
+NEZHA_SERVER=${NEZHA_SERVER}
+NEZHA_PORT=${NEZHA_PORT}
+NEZHA_KEY=${NEZHA_KEY}
+EOF
+    devil www add ${USERNAME}.serv00.net php > /dev/null 2>&1
+    devil www add keep.${USERNAME}.serv00.net nodejs /usr/local/bin/node18 > /dev/null 2>&1
+    devil ssl www add $HOST_IP le le keep.${USERNAME}.serv00.net > /dev/null 2>&1
+    ln -fs /usr/local/bin/node18 ~/bin/node > /dev/null 2>&1
+    ln -fs /usr/local/bin/npm18 ~/bin/npm > /dev/null 2>&1
+    mkdir -p ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    echo 'export PATH=~/.npm-global/bin:~/bin:$PATH' >> $HOME/.bash_profile && source $HOME/.bash_profile
+    rm -rf $HOME/.npmrc > /dev/null 2>&1
+    cd ${keep_path} && npm install dotenv axios --silent > /dev/null 2>&1
+    rm $HOME/domains/keep.${USERNAME}.serv00.net/public_nodejs/public/index.html > /dev/null 2>&1
+    devil www options keep.${USERNAME}.serv00.net sslonly on > /dev/null 2>&1
+    if devil www restart keep.${USERNAME}.serv00.net 2>&1 | grep -q "succesfully"; then
+        echo -e "\e[1;32m\n全自动保活服务安装成功\n\e[0m"
+        echo -e "\e[1;32m=======================================================\e[0m"
+        echo -e "\e[1;35m\n访问 https://keep.${USERNAME}.serv00.net/status 查看进程状态\n\e[0m"
+        echo -e "\e[1;33m访问 https://keep.${USERNAME}.serv00.net/start 调起保活程序\n\e[0m"
+        echo -e "\e[1;35m访问 https://keep.${USERNAME}.serv00.net/list 全部进程列表\n\e[0m"
+        echo -e "\e[1;35m访问 https://keep.${USERNAME}.serv00.net/stop 结束进程和保活\n\e[0m"
+        echo -e "\e[1;32m=======================================================\e[0m"
+        echo -e "\e[1;33m如发现掉线访问https://keep.${USERNAME}.serv00.net/start唤醒,或者用https://console.cron-job.org在线访问网页自动唤醒\n\e[0m"
+        echo -e "\e[1;35m如果需要Telegram通知，请先在Telegram @Botfather 申请 Bot-Token，并带CHAT_ID和BOT_TOKEN环境变量运行\n\n\e[0m"
+        
+    else
+        echo -e "\e[1;91m全自动保活服务安装失败,请删除所有文件夹后重试\n\e[0m"
+    fi
+}
+
+run() {
+  if [ -e "$(basename ${FILE_MAP[npm]})" ]; then
+    tlsPorts=("443" "8443" "2096" "2087" "2083" "2053")
+    if [[ "${tlsPorts[*]}" =~ "${NEZHA_PORT}" ]]; then
+      NEZHA_TLS="--tls"
+    else
+      NEZHA_TLS=""
+    fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+      export TMPDIR=$(pwd)
+      nohup ./"$(basename ${FILE_MAP[npm]})" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+      sleep 1
+      pgrep -x "$(basename ${FILE_MAP[npm]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[npm]}) is running\e[0m" || { echo -e "\e[1;35m$(basename ${FILE_MAP[npm]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[npm]})" && nohup ./"$(basename ${FILE_MAP[npm]})" -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m"$(basename ${FILE_MAP[npm]})" restarted\e[0m"; }
+    else
+      echo -e "\e[1;35mNEZHA variable is empty, skipping running\e[0m"
+    fi
+  fi
+
+  if [ -e "$(basename ${FILE_MAP[web]})" ]; then
+    nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 &
+    sleep 1
+    pgrep -x "$(basename ${FILE_MAP[web]})" > /dev/null && echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) is running\e[0m" || { echo -e "\e[1;35m$(basename ${FILE_MAP[web]}) is not running, restarting...\e[0m"; pkill -f "$(basename ${FILE_MAP[web]})" && nohup ./"$(basename ${FILE_MAP[web]})" -c config.json >/dev/null 2>&1 & sleep 2; echo -e "\e[1;32m$(basename ${FILE_MAP[web]}) restarted\e[0m"; }
+  fi
+rm -rf "$(basename ${FILE_MAP[web]})" "$(basename ${FILE_MAP[npm]})"
+}
+run
+
+get_ip() {
+  IP_LIST=($(devil vhost list | awk '/^[0-9]+/ {print $1}'))
+  API_URL="https://status.eooce.com/api"
+  IP=""
+  THIRD_IP=${IP_LIST[2]}
+  RESPONSE=$(curl -s --max-time 2 "${API_URL}/${THIRD_IP}")
+  if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
+      IP=$THIRD_IP
+  else
+      FIRST_IP=${IP_LIST[0]}
+      RESPONSE=$(curl -s --max-time 2 "${API_URL}/${FIRST_IP}")
+      
+      if [[ $(echo "$RESPONSE" | jq -r '.status') == "Available" ]]; then
+          IP=$FIRST_IP
+      else
+          IP=${IP_LIST[1]}
+      fi
+  fi
+echo "$IP"
+}
+
+HOST_IP=$(get_ip)
+echo -e "\e[1;32m本机IP: $HOST_IP\033[0m"
+
+ISP=$(curl -s --max-time 2 https://speed.cloudflare.com/meta | awk -F\" '{print $26}' | sed -e 's/ /_/g' || echo "0")
+get_name() { if [ "$HOSTNAME" = "s1.ct8.pl" ]; then SERVER="CT8"; else SERVER=$(echo "$HOSTNAME" | cut -d '.' -f 1); fi; echo "$SERVER"; }
+NAME=$ISP-$(get_name)-tuic
+
+echo -e "\e[1;32mTuic安装成功\033[0m\n"
+echo -e "\e[1;33mV2rayN 或 Nekobox等直接可以导入使用,跳过证书验证需设置为true\033[0m\n"
+cat > ${FILE_PATH}/${SUB_TOKEN}_tuic.log <<EOF
+tuic://$UUID:$PASSWORD@$HOST_IP:$PORT?congestion_control=bbr&alpn=h3&sni=www.bing.com&udp_relay_mode=native&allow_insecure=1#$NAME
+EOF
+cat ${FILE_PATH}/${SUB_TOKEN}_tuic.log
+echo -e "\n\e[1;33mClash: \033[0m"
+cat << EOF
+- name: $NAME
+  type: tuic
+  server: $HOST_IP
+  port: $PORT                                                          
+  uuid: $UUID
+  password: $PASSWORD
+  alpn: [h3]
+  disable-sni: true
+  reduce-rtt: true
+  udp-relay-mode: native
+  congestion-controller: bbr
+  sni: www.bing.com                                
+  skip-cert-verify: true
+EOF
+echo -e "\n\e[1;35m节点订阅链接: https://${USERNAME}.serv00.net/${SUB_TOKEN}_tuic.log  适用于V2ranN/Nekobox/Karing/小火箭/sterisand/Loon 等\033[0m\n"
+rm -rf config.json fake_useragent_0.2.0.json
+install_keepalive
+echo -e "\e[1;35m老王serv00|CT8单协议tuic无交互一键安装脚本[0m"
+echo -e "\e[1;35m脚本地址：https://github.com/eooce/scripts\e[0m"
+echo -e "\e[1;35m反馈论坛：https://bbs.vps8.me\e[0m"
+echo -e "\e[1;35mTG反馈群组：https://t.me/vps888\e[0m"
+echo -e "\e[1;35m转载请著名出处，请勿滥用\e[0m\n"
+echo -e "\e[1;32mRuning done!\033[0m"
